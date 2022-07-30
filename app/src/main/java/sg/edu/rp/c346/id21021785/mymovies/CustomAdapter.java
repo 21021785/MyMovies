947cf4d1a64cecb6ae863adc948fca8f826f6e2b
21021785 <21021785@myrp.edu.sg>
2022-07-30 23:58:45 +0800

    showMovies

--- app/src/main/java/sg/edu/rp/c346/id21021785/mymovies/CustomAdapter.java
+++ app/src/main/java/sg/edu/rp/c346/id21021785/mymovies/CustomAdapter.java
@@ -5,11 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class CustomAdapter {
+public class CustomAdapter extends ArrayAdapter{
     Context parent_context;
     int layout_id;
     ArrayList<Movie> movieList;
